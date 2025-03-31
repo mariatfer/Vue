64a848698af76df29010bdf2b8dd1621a8ab455f
María Fernández <matefe1706@gmail.com>
2025-03-31 22:38:51 +0200

    feat(components): added parent and children components

---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ChildComponent from '@/components/ChildComponent.vue'
+
+function sayHiEvent(response: { message: string }) {
+  console.log('Children message: ', response)
+}
+</script>
+
+<template>
+  <ChildComponent @sayHi="sayHiEvent" />
+</template>
+
+<style scoped lang="scss"></style>
