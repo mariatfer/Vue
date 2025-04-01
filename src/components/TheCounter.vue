69ac78c4fd3ef136f30fbe0465cde46f795e608a
María Fernández <matefe1706@gmail.com>
2025-04-01 08:45:00 +0200

    feat(components): added a computed function)

--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { useCounter } from '@/composables/useCounter'
+import { computed } from 'vue'
 
 const { count, increment, decrement, isCountAboveZero, isCountBelowTen } = useCounter()
+
+const countMultiplied = computed(() => {
+  return count.value * 2;
+})
+
 </script>
 
 <template>
-  <div :class="{'counter__active': count === 10 }" class="counter">
+  <div :class="{ counter__active: count === 10 }" class="counter">
     <h2>Counter: {{ count }}</h2>
+    <h2>Counter * 2: {{ countMultiplied }} </h2>
     <button class="counter__button" @click="increment" v-if="isCountBelowTen()">Increment</button>
     <button class="counter__button" @click="decrement" v-if="isCountAboveZero()">Decrement</button>
   </div>
